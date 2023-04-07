8e0dfc69b96ff4daccedadc21b169a0408f96bd1
Atanu-bhowmik <atanubhowmik51@gmail.com>
2023-04-07 11:31:12 +0530

    state and local added

---
+++ state.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+contract state{
+    uint public age;
+
+    //------set value usiing constructor
+
+    // constructor() public{
+    //     age=10;
+    // }
+
+    // ------------set value using setter
+
+    // function SetAge() public {
+    //     age=10;
+    // }
+}
