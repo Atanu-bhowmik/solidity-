8e5aa0bf71accda228a8d15931ccd5f0ba107670
Atanu-bhowmik <atanubhowmik51@gmail.com>
2023-04-07 14:02:34 +0530

    array,dynamic array,byte array

---
+++ array.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0<0.9.0.;
+contract Array {
+
+    //---------static array
+
+    // uint[4] public arr=[10,20,30,40];
+    // function setter (uint index,uint value) public{
+    //     arr[index]=value;
+    // }
+    // function length() public view returns(uint){
+    //     return arr.length;
+    // }
+
+    //---------dynamic array
+
+    // uint[] public arr;
+    // function pushElement(uint element) public{
+    //     arr.push(element);
+    // }
+    // function popElement() public {
+    //     arr.pop();
+    // }
+
+    //------byte array
+
+    // bytes3 public b3;
+    // bytes2 public b2;
+
+    // function setter() public{
+    //     b3='abc';
+    //     b2='ab';
+    // }
+    // bytes arrays are immutable
+
+    //--dynamic byte array
+    bytes public b1='abc';
+    function pushElement() public{
+        b1.push('d');
+    }
+    function getElement(uint i) public view returns(bytes1){
+        return b1[i];
+    }
+    function getLenth() public view returns(uint){
+        return b1.length;
+    }
+
+
+
+}
