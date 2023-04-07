2908feabb10c45f4929910b3e762e1fa042d63be
Atanu-bhowmik <atanubhowmik51@gmail.com>
2023-04-07 12:56:26 +0530

    getter and setter

--- function.sol
+++ function.sol
@@ -3,8 +3,8 @@
 pragma solidity >=0.8.2 <0.9.0;
 contract function2 {
     uint age;
-    function setAge() public{
-        age=21;
+    function setAge(uint newage) public{
+        age=newage;
     }
     function getAge() public view returns(uint){
         return age;
